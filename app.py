0999414b64ffe6109013d7629a4d8e55cca796ea
Delphine Tai-Beauchamp <delphinetaibeauchamp@gmail.com>
2024-09-08 18:01:16 -0700

    cutting and merging successful

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
-from asyncio.log import logger
-from fastapi import FastAPI, File, UploadFile, Form
-from main import cut_video
+from loguru import logger
+from typing import List
+from uuid import uuid4
+
+from fastapi import FastAPI, File, UploadFile, Form, HTTPException
+from main import cut_video, merge_segments
 import os
 
 app = FastAPI()
+
+UPLOAD_DIR = "uploads"
 
 
 @app.post("/cut-video/")
 async def cut_video_endpoint(
     file: UploadFile = File(...),
-    start_time: str = Form(...),
-    end_time: str = Form(...)
-    ):
+    start_times: List[str] = Form(...),
+    end_times: List[str] = Form(...)
+):
+
+    if len(start_times) != len(end_times):
+        logger.error("Start times and end times must have the same length")
+        raise HTTPException(status_code=400, detail="Start times and end times must have the same length")
 
     upload_dir = "uploads"
     if not os.path.exists(upload_dir):
         os.mkdir(upload_dir)
+
     # save video locally
-    video_path = os.path.join(upload_dir, file.filename)
+    video_path = os.path.join(UPLOAD_DIR, f"{uuid4()}.mp4")
 
     # write as binary since video
     with open(video_path, "wb") as buffer:  # store temporarily
@@ -25,6 +35,13 @@ async def cut_video_endpoint(
 
     logger.info(f"Cutting video {video_path}")
 
-    # call video cutting function
-    output_path = cut_video(video_path, start_time, end_time)
-    return {"message": "Video cut successfully!", "output": output_path}
+    # cut video segments
+    video_segment_paths = []
+    for i, (start_time, end_time) in enumerate(zip(start_times, end_times)):
+        video_segment_path = f"uploads/segment_{i}_{start_time.replace(':', '-')}_{end_time.replace(':', '-')}.mp4"
+        cut_video(video_path, start_time, end_time, video_segment_path)
+        video_segment_paths.append(video_segment_path)
+
+    merged_video_path = merge_segments(video_segment_paths)
+
+    return {"message": "Video cut and merged successfully!", "output": merged_video_path}
--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+import os
+from datetime import datetime
+from typing import List
+from uuid import uuid4
+
 import ffmpeg
 
 
-def cut_video(input_path: str, start: str, end: str) -> str:
-    output_path = input_path.replace(".mp4", "_cut.mp4")
+def cut_video(input_path: str, start: str, end: str, output_path: str) -> str:
     ffmpeg.input(input_path, ss=start, to=end).output(output_path).run()
     return output_path
+
+
+def merge_segments(segment_paths: List[str]) -> str:
+    list_file = f"uploads/{uuid4()}.txt"
+
+    with open(list_file, "w") as f:
+        for segment in segment_paths:
+            f.write(f"file '{os.path.abspath(segment)}'\n")
+
+    merged_output = merged_output = f"uploads/merged_video_{datetime.now().strftime('%Y%m%d_%H%M%S')}.mp4"
+
+    # merge
+    ffmpeg.input(list_file, format='concat', safe=0).output(merged_output, c='copy').run()
+
+    return merged_output
