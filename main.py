d53efef16fbad6a7edbd814aab6d30e650c98ccc
Delphine Tai-Beauchamp <delphinetaibeauchamp@gmail.com>
2024-09-11 00:33:45 -0700

    test

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 from datetime import datetime
 from typing import List
 from uuid import uuid4
-
 import ffmpeg
+from fastapi import HTTPException
 
 
 def cut_video(input_path: str, start: str, end: str, output_path: str) -> str:
@@ -12,15 +12,29 @@ def cut_video(input_path: str, start: str, end: str, output_path: str) -> str:
 
 
 def merge_segments(segment_paths: List[str]) -> str:
+    if not segment_paths:
+        raise ValueError("No segments provided for merging.")
+
     list_file = f"uploads/{uuid4()}.txt"
+    merged_output = f"uploads/merged_video_{datetime.now().strftime('%Y%m%d_%H%M%S')}.mp4"
 
-    with open(list_file, "w") as f:
+    try:
+        with open(list_file, "w") as f:
+            for segment in segment_paths:
+                f.write(f"file '{os.path.abspath(segment)}'\n")
+
+        # merge
+        ffmpeg.input(list_file, format='concat', safe=0).output(merged_output, c='copy').run()
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error merging segments: {str(e)}")
+
+    finally:
+        # clean
+        if os.path.exists(list_file):
+            os.remove(list_file)
         for segment in segment_paths:
-            f.write(f"file '{os.path.abspath(segment)}'\n")
-
-    merged_output = merged_output = f"uploads/merged_video_{datetime.now().strftime('%Y%m%d_%H%M%S')}.mp4"
-
-    # merge
-    ffmpeg.input(list_file, format='concat', safe=0).output(merged_output, c='copy').run()
+            if os.path.exists(segment):
+                os.remove(segment)
 
     return merged_output
